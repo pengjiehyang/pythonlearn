6e50a666c376031eafc20c8d5ee00994af764e84
Jackyang <pengjiehyang@outlook.com>
2025-07-18 10:14:02 +0800

    v0.8

--- main.py
+++ main.py
@@ -43,23 +43,25 @@ while True:
     except ValueError:
           print("請輸入有效的整數")        
  elif 選擇 == "2":
-     try:
+     模式 =input("請選擇模式:(1)1-100模式(2)自定義模式:")
+     if 模式 == "1":
+      try:
          print("歡迎來到猜數字(按下0退出)")
          亂數 = random.randint(1, 100)
          紀錄次數 = 0
          目標次數=(int(input("請先設定目標次數")))
-     except ValueError:
-                print("請輸入有效整數")   
-     while True: 
-      猜數字 = (int(input ("請輸入數字:")))
-      if 猜數字 == 0:
-       print("已退出")
-       break   
-      elif 猜數字 == 亂數:
-       紀錄次數 += 1
-       print(f"恭喜你猜對了，你贏了！{紀錄次數}次猜中")
-       break
-      elif 猜數字 < 亂數:
+      except ValueError:
+        print("請輸入有效整數")   
+      while True: 
+       猜數字 = (int(input ("請輸入數字:")))
+       if 猜數字 == 0:
+        print("已退出")
+        break   
+       elif 猜數字 == 亂數:
+        紀錄次數 += 1
+        print(f"恭喜你猜對了，你贏了！{紀錄次數}次猜中")
+        break
+       elif 猜數字 < 亂數:
         紀錄次數 += 1
         print("你猜的字太小了，請再試一次(按下0退出):")   
         剩下次數 = 目標次數 - 紀錄次數
@@ -68,7 +70,7 @@ while True:
           剩下次數 = 目標次數 - 紀錄次數
           print(f"你輸了。")
           break
-      elif 猜數字 > 亂數:
+       elif 猜數字 > 亂數:
          紀錄次數 += 1
          print("你猜的數字太大了，請再試一次(按下0退出):")
          剩下次數 = 目標次數 - 紀錄次數
@@ -77,13 +79,64 @@ while True:
           剩下次數 = 目標次數 - 紀錄次數
           print(f"你輸了。")
           break
-      elif 紀錄次數 ==目標次數 or 紀錄次數 > 目標次數:
+       elif 紀錄次數 ==目標次數 or 紀錄次數 > 目標次數:
         剩下次數 = 目標次數 - 紀錄次數
         print(f"你輸了。")
         break
-      else:
+       else:
         print("無效的輸入，請輸入1-目標次數0的數字或0退出")
-        猜數字 = input("請輸入1-目標次數0的數字(按下0退出):")
+        猜數字 = input("請輸入數字按下0退出):")
+     if 模式 == "2": 
+      try:
+        print("歡迎來到猜數字請選擇數字範圍(按下0退出)")
+        數字1 =(int(input("請輸入開始的數字(頭)")))
+        if 數字1 == "0": 
+           print("已退出")
+           break
+        數字2 =(int(input("請輸入結束的數字(尾)")))
+        if 數字2 == "0":
+          print("已退出")
+          break
+          print("歡迎來到猜數字(按下0退出)")
+        亂數 = random.randint(數字1, 數字2)
+        紀錄次數 = 0
+        目標次數=(int(input("請先設定目標次數")))
+      except ValueError:
+         print("請輸入有效整數")    
+      while True: 
+         猜數字 = (int(input ("請輸入數字:")))
+         if 猜數字 == 0:
+          print("已退出")
+          break   
+         elif 猜數字 == 亂數:
+          紀錄次數 += 1
+          print(f"恭喜你猜對了，你贏了！{紀錄次數}次猜中")
+          break
+         elif 猜數字 < 亂數:
+          紀錄次數 += 1
+          print("你猜的字太小了，請再試一次(按下0退出):")   
+          剩下次數 = 目標次數 - 紀錄次數
+          print(f"你目前已經猜了{紀錄次數}次，還剩下{剩下次數}次")
+         elif 紀錄次數 ==目標次數 or 紀錄次數 > 目標次數:
+          剩下次數 = 目標次數 - 紀錄次數
+          print(f"你輸了。")
+          break
+         elif 猜數字 > 亂數:
+          紀錄次數 += 1
+          print("你猜的數字太大了，請再試一次(按下0退出):")
+          剩下次數 = 目標次數 - 紀錄次數
+          print(f"你目前已經猜了{紀錄次數}次，還剩下{剩下次數}次")
+         elif 紀錄次數 ==目標次數 or 紀錄次數 > 目標次數:
+           剩下次數 = 目標次數 - 紀錄次數
+           print(f"你輸了。")
+           break
+         elif 紀錄次數 ==目標次數 or 紀錄次數 > 目標次數:
+            剩下次數 = 目標次數 - 紀錄次數
+            print(f"你輸了。")
+            break
+         else:
+           print("無效的輸入")
+           猜數字 = input("請再次輸入")   
  elif 選擇 == "3":
     while True:
      開始=input("歡迎來到擲骰子按下y開始，按下n離開").lower()
